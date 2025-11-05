d3f39e78e8eabbd74cca9f86931093325a77c600
kauansoaresalmeida <kauansoa999@gmail.com>
2025-11-05 19:27:47 -0300

    05/11

---
+++ Classe/Operacoes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo.Classes
+{
+    internal class Operacoes
+    {
+        double x, y;
+        public Operacoes()
+        {
+            x = 0; y = 0;
+        }
+
+        //Getters e Setters
+
+        public void setXFromInput(double x)
+        {
+            this.x = x;
+        }
+
+        public double getX()
+        {
+            return this.x;
+        }
+        public double getY()
+        {
+            return this.y;
+        }
+        public void setYFromInput(double y)
+        {
+            this.y = y;
+        }
+
+        public double soma()
+        {
+            return this.x + this.y;
+        }
+        public double subtracao()
+        {
+            return this.x - this.y;
+
+        }
+
+        public double multiplicacao()
+        {
+            return this.x * this.y;
+        }
+
+        public double divisao()
+        {
+            return this.x / this.y;
+        }
+
+        public double valorQuadrado()
+        {
+            return this.x * this.x;
+        }
+
+        public double raizQuadrada()
+        {
+            return this.x / this.x;
+        }
+
+        public double porcentagem()
+        {
+            return (this.x / 100) * this.y;
+        }
+
+        public int binario()
+        {
+            int num = (int)this.x;
+            return  int.Parse(Convert.ToString(num, 2));
+
+        }
+
+        public double raizY()
+        { 
+            return Math.Pow(this.x, this.y);
+       
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+using Exemplo.Classes;
+using System.Configuration;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Exemplo
+{
+    public partial class Form1 : Form
+    {
+        Operacoes op;
+        public Form1()
+        {
+            InitializeComponent();
+            op = new Operacoes();
+            
+        }
+        public void setInputs()
+        {
+            op.setXFromInput(double.Parse(inputTextX.Text));
+            op.setYFromInput(double.Parse(inputTextY.Text));
+
+        }
+
+        public void printResult1(double x, double y, string operacao, double result)
+        {
+            string formatoSaida = " "+ x+operacao + " = ";
+            labelOutput.Visible = true;
+            labelOutput.Text = formatoSaida;
+
+            string resultado = "" + result + "";
+            labelOutput2.Visible = true;
+            labelOutput2.Text = resultado;
+        }
+
+        public void printResult2(double x, double y, string operacao, double result)
+        {
+            string formatoSaida = " " + x + operacao + " = ";
+            labelOutput.Visible = true;
+            labelOutput.Text = formatoSaida;
+
+            string resultado = "" + result + "";
+            labelOutput2.Visible = true;
+            labelOutput2.Text = resultado;
+
+        }
+
+        public void printResult3(double x, double y, string operacao, double result)
+        {
+            string formatoSaida = " "+ operacao + x + " = ";
+            labelOutput.Visible = true;
+            labelOutput.Text = formatoSaida;
+
+            string resultado = "" + result + "";
+            labelOutput2.Visible = true;
+            labelOutput2.Text = resultado;
+
+        }
+        public void printResult(double x, double y, string operacao, double result)
+        {
+            string formatoSaida = ""+ x + operacao + y + " = ";
+            labelOutput.Visible = true;
+            labelOutput.Text = formatoSaida;
+
+            string resultado = ""+result+"";
+            labelOutput2.Visible = true;
+            labelOutput2.Text = resultado;
+        }
+
+        public void printResult4(double x, double y, string operacao, double result)
+        {
+            string formatoSaida = "" + y + operacao + x + " = ";
+            labelOutput.Visible = true;
+            labelOutput.Text = formatoSaida;
+
+            string resultado = "" + result + "";
+            labelOutput2.Visible = true;
+            labelOutput2.Text = resultado;
+        }
+        public void onBtnSoma_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.soma();
+            printResult(op.getX(), op.getY(), " + ", result);
+        }
+
+        public void onBtnSubtracao_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.subtracao();
+            printResult(op.getX(), op.getY(), " - ", result);
+        }
+
+        public void onBtnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.multiplicacao();
+            printResult(op.getX(), op.getY(), " x ", result);
+        }
+
+        public void onBtnDivisao_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.divisao();
+            printResult(op.getX(), op.getY(), " / ", result);
+        }
+
+        public void onBtnvalorQuadrado_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.valorQuadrado();
+            printResult1(op.getX(), op.getX(), "^²", result);
+        }
+
+        public void onBtnraizQuadrada_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.raizQuadrada();
+            printResult3(op.getX(), op.getX(), " Raiz quadrada de ", result);
+        }
+
+        public void onBtnPorcentagem_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.porcentagem();
+            printResult4(op.getX(), op.getY(), "% de ", result);
+        }
+
+        public void onBtnBinario_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.binario();
+            printResult1(op.getX(), op.getY(), " em binario é ", result);
+        }
+
+        public void onBtnRaizY_Click(object sender, EventArgs e)
+        {
+            setInputs();
+            double result = op.raizY();
+            printResult(op.getX(), op.getY(), " elevado á ", result);
+        }
+
+     
+
+    }
+}
